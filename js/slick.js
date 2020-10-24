4cb64bd9ec806d97b9b0604e9bb7ee725666cc11
Denis Melnik <frntprog@gmail.com>
2020-10-24 22:42:29 +0300

    Initial commit

---
+++ js/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 4000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+  });
+});
